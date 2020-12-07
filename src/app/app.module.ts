99e871c76c51894608abe8b98abf3c4ccc8d2881
Dima Şironin <shironin@live.ru>
2020-12-08 00:20:10 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MainViewComponent } from '@component/main-view/main-view.component';
+import { HeaderComponent} from '@component/header/header.component';
+import { AboutUsComponent} from '@component/about-us/about-us.component';
+import { ServicesComponent} from '@component/services/services.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent,
+    HeaderComponent,
+    AboutUsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
