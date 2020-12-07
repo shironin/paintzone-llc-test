bf0160e5b79bc8e1dddbef93db12bb2b81c46f39
Dima Şironin <shironin@live.ru>
2020-12-08 00:46:02 +0200

    working commit

--- src/app/component/services/services.component.ts
+++ src/app/component/services/services.component.ts
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.scss'],
 })
 export class ServicesComponent {
+  galleryItems = [
+    {
+      text: 'Interior',
+      imageUrl: 'https://www.w3schools.com/w3css/img_lights.jpg'
+    },
+    {
+      text: 'Exterior',
+      imageUrl: 'https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg'
+    },
+    {
+      text: 'Office',
+      imageUrl: 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png'
+    },
+    {
+      text: 'Commercial',
+      imageUrl: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'
+    },
+    {
+      text: 'Residential',
+      imageUrl: 'https://image.freepik.com/free-photo/double-exposure-image-business-profit-growth_31965-3022.jpg'
+    }
+  ];
 
   constructor() {
   }
